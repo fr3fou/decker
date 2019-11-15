26bf5e53d695ac06b5ffabb59d6b2c7adfbb0381
Simo Aleksandrov <simo3003@me.com>
2019-11-15 16:17:48 +0200

    hash

--- decker.go
+++ decker.go
@@ -1,21 +1,41 @@
 package decker
 
 import (
+	"encoding/binary"
+	"fmt"
 	"image"
-"github.com/corona10/goimagehash"
+	"log"
+
+	"github.com/corona10/goimagehash"
+	"github.com/pkg/errors"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // Decker is the main struct of the app
 type Decker struct {
-	Images    []image.Image
+	Images    map[string]image.Image
 	DB        *leveldb.DB
 	Threshold int
 }
 
-// Hash takes the perceptual hash of every image in the array and adds it it to the DB
-func (d *Decker) Hash()  {
-	for _, img := range d.Images {
-		hash, err := goimagehash.PerceptualHash(img)
-	}	
+// Hash takes the perception hash of every image in the array and adds it it to the DB
+func (d *Decker) Hash() {
+	if d.DB == nil {
+		panic("decker: leveldb.DB instance not provided")
+	}
+
+	for path, img := range d.Images {
+		hash, err := goimagehash.PerceptionHash(img)
+
+		if err != nil {
+			log.Println(errors.Wrap(err, fmt.Sprintf("image %s couldn't be hashed", img)))
+		}
+
+		// Make the hash into a byte array
+		b := make([]byte, 8)
+		binary.LittleEndian.PutUint64(b, hash.GetHash())
+
+		// Put into the DB
+		d.DB.Put([]byte(path), b, nil)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/corona10/goimagehash v1.0.1
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/syndtr/goleveldb v1.0.0
 )
