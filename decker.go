384d1646bf9175c87940ae77811a4786c55d00d7
Simo Aleksandrov <simo3003@me.com>
2019-11-15 15:39:57 +0200

    begin working on decker

--- decker.go
+++ decker.go
@@ -1,2 +1,21 @@
 package decker
 
+import (
+	"image"
+"github.com/corona10/goimagehash"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+// Decker is the main struct of the app
+type Decker struct {
+	Images    []image.Image
+	DB        *leveldb.DB
+	Threshold int
+}
+
+// Hash takes the perceptual hash of every image in the array and adds it it to the DB
+func (d *Decker) Hash()  {
+	for _, img := range d.Images {
+		hash, err := goimagehash.PerceptualHash(img)
+	}	
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/fr3fou/decker-cli
 
 go 1.13
+
+require (
+	github.com/corona10/goimagehash v1.0.1
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/syndtr/goleveldb v1.0.0
+)
