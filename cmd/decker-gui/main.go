545aaac80a85cb90aec8173812376a56e2318653
Simo Aleksandrov <simo3003@me.com>
2020-09-30 22:07:51 +0300

    add gui dep

--- cmd/decker-gui/main.go
+++ cmd/decker-gui/main.go
@@ -1,7 +1,373 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
 package main
 
-import "fmt"
+// A Gio program that demonstrates Gio widgets. See https://gioui.org for more information.
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	"gioui.org/app"
+	"gioui.org/app/headless"
+	"gioui.org/f32"
+	"gioui.org/font/gofont"
+	"gioui.org/io/router"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+
+	"golang.org/x/exp/shiny/materialdesign/icons"
+)
+
+var screenshot = flag.String("screenshot", "", "save a screenshot to a file and exit")
+var disable = flag.Bool("disable", false, "disable all widgets")
+
+type iconAndTextButton struct {
+	theme  *material.Theme
+	button *widget.Clickable
+	icon   *widget.Icon
+	word   string
+}
 
 func main() {
-	fmt.Println("gui")
+	flag.Parse()
+	editor.SetText(longText)
+	ic, err := widget.NewIcon(icons.ContentAdd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	icon = ic
+	progressIncrementer = make(chan int)
+	if *screenshot != "" {
+		if err := saveScreenshot(*screenshot); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to save screenshot: %v\n", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			progressIncrementer <- 10
+		}
+	}()
+
+	go func() {
+		w := app.NewWindow(app.Size(unit.Dp(800), unit.Dp(700)))
+		if err := loop(w); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
 }
+
+func saveScreenshot(f string) error {
+	const scale = 1.5
+	sz := image.Point{X: 800 * scale, Y: 600 * scale}
+	w, err := headless.NewWindow(sz.X, sz.Y)
+	if err != nil {
+		return err
+	}
+	gtx := layout.Context{
+		Ops: new(op.Ops),
+		Metric: unit.Metric{
+			PxPerDp: scale,
+			PxPerSp: scale,
+		},
+		Constraints: layout.Exact(sz),
+		Queue:       new(router.Router),
+	}
+	th := material.NewTheme(gofont.Collection())
+	kitchen(gtx, th)
+	w.Frame(gtx.Ops)
+	img, err := w.Screenshot()
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	if err := png.Encode(&buf, img); err != nil {
+		return err
+	}
+	return ioutil.WriteFile(f, buf.Bytes(), 0666)
+}
+
+func loop(w *app.Window) error {
+	th := material.NewTheme(gofont.Collection())
+
+	var ops op.Ops
+	for {
+		select {
+		case e := <-w.Events():
+			switch e := e.(type) {
+			case system.ClipboardEvent:
+				lineEditor.SetText(e.Text)
+			case system.DestroyEvent:
+				return e.Err
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
+				for iconButton.Clicked() {
+					w.WriteClipboard(lineEditor.Text())
+				}
+				for flatBtn.Clicked() {
+					w.ReadClipboard()
+				}
+				if *disable {
+					gtx = gtx.Disabled()
+				}
+				if checkbox.Changed() {
+					if checkbox.Value {
+						transformTime = e.Now
+					} else {
+						transformTime = time.Time{}
+					}
+				}
+
+				transformedKitchen(gtx, th)
+				e.Frame(gtx.Ops)
+			}
+		case p := <-progressIncrementer:
+			progress += p
+			if progress > 100 {
+				progress = 0
+			}
+			w.Invalidate()
+		}
+	}
+}
+
+func transformedKitchen(gtx layout.Context, th *material.Theme) layout.Dimensions {
+	if !transformTime.IsZero() {
+		dt := float32(gtx.Now.Sub(transformTime).Seconds())
+		angle := dt * .1
+		op.InvalidateOp{}.Add(gtx.Ops)
+		defer op.Push(gtx.Ops).Pop()
+		tr := f32.Affine2D{}
+		tr = tr.Rotate(f32.Pt(300, 20), -angle)
+		scale := 1.0 - dt*.5
+		if scale < 0.5 {
+			scale = 0.5
+		}
+		tr = tr.Scale(f32.Pt(300, 20), f32.Pt(scale, scale))
+		offset := dt * 50
+		if offset > 200 {
+			offset = 200
+		}
+		tr = tr.Offset(f32.Pt(0, offset))
+		op.Affine(tr).Add(gtx.Ops)
+	}
+
+	return kitchen(gtx, th)
+}
+
+var (
+	editor     = new(widget.Editor)
+	lineEditor = &widget.Editor{
+		SingleLine: true,
+		Submit:     true,
+	}
+	button            = new(widget.Clickable)
+	greenButton       = new(widget.Clickable)
+	iconTextButton    = new(widget.Clickable)
+	iconButton        = new(widget.Clickable)
+	flatBtn           = new(widget.Clickable)
+	disableBtn        = new(widget.Clickable)
+	radioButtonsGroup = new(widget.Enum)
+	list              = &layout.List{
+		Axis: layout.Vertical,
+	}
+	progress            = 0
+	progressIncrementer chan int
+	green               = true
+	topLabel            = "Hello, Gio"
+	icon                *widget.Icon
+	checkbox            = new(widget.Bool)
+	swtch               = new(widget.Bool)
+	transformTime       time.Time
+	float               = new(widget.Float)
+)
+
+type (
+	D = layout.Dimensions
+	C = layout.Context
+)
+
+func (b iconAndTextButton) Layout(gtx layout.Context) layout.Dimensions {
+	return material.ButtonLayout(b.theme, b.button).Layout(gtx, func(gtx C) D {
+		return layout.UniformInset(unit.Dp(12)).Layout(gtx, func(gtx C) D {
+			iconAndLabel := layout.Flex{Axis: layout.Horizontal, Alignment: layout.Middle}
+			textIconSpacer := unit.Dp(5)
+
+			layIcon := layout.Rigid(func(gtx C) D {
+				return layout.Inset{Right: textIconSpacer}.Layout(gtx, func(gtx C) D {
+					var d D
+					if icon != nil {
+						size := gtx.Px(unit.Dp(56)) - 2*gtx.Px(unit.Dp(16))
+						b.icon.Layout(gtx, unit.Px(float32(size)))
+						d = layout.Dimensions{
+							Size: image.Point{X: size, Y: size},
+						}
+					}
+					return d
+				})
+			})
+
+			layLabel := layout.Rigid(func(gtx C) D {
+				return layout.Inset{Left: textIconSpacer}.Layout(gtx, func(gtx C) D {
+					l := material.Body1(b.theme, b.word)
+					l.Color = b.theme.Color.InvText
+					return l.Layout(gtx)
+				})
+			})
+
+			return iconAndLabel.Layout(gtx, layIcon, layLabel)
+		})
+	})
+}
+
+func kitchen(gtx layout.Context, th *material.Theme) layout.Dimensions {
+	for _, e := range lineEditor.Events() {
+		if e, ok := e.(widget.SubmitEvent); ok {
+			topLabel = e.Text
+			lineEditor.SetText("")
+		}
+	}
+	widgets := []layout.Widget{
+		material.H3(th, topLabel).Layout,
+		func(gtx C) D {
+			gtx.Constraints.Max.Y = gtx.Px(unit.Dp(200))
+			return material.Editor(th, editor, "Hint").Layout(gtx)
+		},
+		func(gtx C) D {
+			e := material.Editor(th, lineEditor, "Hint")
+			e.Font.Style = text.Italic
+			border := widget.Border{Color: color.RGBA{A: 0xff}, CornerRadius: unit.Dp(8), Width: unit.Px(2)}
+			return border.Layout(gtx, func(gtx C) D {
+				return layout.UniformInset(unit.Dp(8)).Layout(gtx, e.Layout)
+			})
+		},
+		func(gtx C) D {
+			in := layout.UniformInset(unit.Dp(8))
+			return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
+				layout.Rigid(func(gtx C) D {
+					return in.Layout(gtx, material.IconButton(th, iconButton, icon).Layout)
+				}),
+				layout.Rigid(func(gtx C) D {
+					return in.Layout(gtx, iconAndTextButton{theme: th, icon: icon, word: "Icon", button: iconTextButton}.Layout)
+				}),
+				layout.Rigid(func(gtx C) D {
+					return in.Layout(gtx, func(gtx C) D {
+						for button.Clicked() {
+							green = !green
+						}
+						return material.Button(th, button, "Click me!").Layout(gtx)
+					})
+				}),
+				layout.Rigid(func(gtx C) D {
+					return in.Layout(gtx, func(gtx C) D {
+						l := "Green"
+						if !green {
+							l = "Blue"
+						}
+						btn := material.Button(th, greenButton, l)
+						if green {
+							btn.Background = color.RGBA{A: 0xff, R: 0x9e, G: 0x9d, B: 0x24}
+						}
+						return btn.Layout(gtx)
+					})
+				}),
+				layout.Rigid(func(gtx C) D {
+					return in.Layout(gtx, func(gtx C) D {
+						return material.Clickable(gtx, flatBtn, func(gtx C) D {
+							return layout.UniformInset(unit.Dp(12)).Layout(gtx, func(gtx C) D {
+								flatBtnText := material.Body1(th, "Flat")
+								if gtx.Queue == nil {
+									flatBtnText.Color.A = 150
+								}
+								return layout.Center.Layout(gtx, flatBtnText.Layout)
+							})
+						})
+					})
+				}),
+			)
+		},
+		material.ProgressBar(th, progress).Layout,
+		func(gtx C) D {
+			return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
+				layout.Rigid(
+					material.CheckBox(th, checkbox, "Transform").Layout,
+				),
+				layout.Rigid(func(gtx C) D {
+					return layout.Inset{Left: unit.Dp(16)}.Layout(gtx,
+						material.Switch(th, swtch).Layout,
+					)
+				}),
+				layout.Rigid(func(gtx C) D {
+					return layout.Inset{Left: unit.Dp(16)}.Layout(gtx, func(gtx C) D {
+						text := "enabled"
+						if !swtch.Value {
+							text = "disabled"
+							gtx = gtx.Disabled()
+						}
+						btn := material.Button(th, disableBtn, text)
+						return btn.Layout(gtx)
+					})
+				}),
+				layout.Rigid(func(gtx C) D {
+					return layout.Inset{Left: unit.Dp(16)}.Layout(gtx, func(gtx C) D {
+						if !swtch.Value {
+							return D{}
+						}
+						return material.Loader(th).Layout(gtx)
+					})
+				}),
+			)
+		},
+		func(gtx C) D {
+			return layout.Flex{}.Layout(gtx,
+				layout.Rigid(material.RadioButton(th, radioButtonsGroup, "r1", "RadioButton1").Layout),
+				layout.Rigid(material.RadioButton(th, radioButtonsGroup, "r2", "RadioButton2").Layout),
+				layout.Rigid(material.RadioButton(th, radioButtonsGroup, "r3", "RadioButton3").Layout),
+			)
+		},
+		func(gtx C) D {
+			return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
+				layout.Flexed(1, material.Slider(th, float, 0, 2*math.Pi).Layout),
+				layout.Rigid(func(gtx C) D {
+					return layout.UniformInset(unit.Dp(8)).Layout(gtx,
+						material.Body1(th, fmt.Sprintf("%.2f", float.Value)).Layout,
+					)
+				}),
+			)
+		},
+	}
+
+	return list.Layout(gtx, len(widgets), func(gtx C, i int) D {
+		return layout.UniformInset(unit.Dp(16)).Layout(gtx, widgets[i])
+	})
+}
+
+const longText = `1. I learned from my grandfather, Verus, to use good manners, and to
+put restraint on anger. 2. In the famous memory of my father I had a
+pattern of modesty and manliness. 3. Of my mother I learned to be
+pious and generous; to keep myself not only from evil deeds, but even
+from evil thoughts; and to live with a simplicity which is far from
+customary among the rich. 4. I owe it to my great-grandfather that I
+did not attend public lectures and discussions, but had good and able
+teachers at home; and I owe him also the knowledge that for things of
+this nature a man should count no expense too great.`
--- go.mod
+++ go.mod
@@ -1,11 +1,10 @@
 module github.com/fr3fou/decker
 
-go 1.13
+go 1.15
 
 require (
+	gioui.org v0.0.0-20200929170715-d2e06d938930
 	github.com/corona10/goimagehash v1.0.2
-	github.com/davecgh/go-spew v1.1.1
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	github.com/pkg/errors v0.8.1
-	github.com/syndtr/goleveldb v1.0.0
+	golang.org/x/exp v0.0.0-20200924195034-c827fd4f18b9
 )
