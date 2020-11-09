08873b963911117dd5bb0bec58e3704258379e3a
Simo Aleksandrov <simo3003@me.com>
2020-11-09 23:39:22 +0200

    Init raygui

--- cmd/decker-gui/main.go
+++ cmd/decker-gui/main.go
@@ -1,282 +1,86 @@
-// SPDX-License-Identifier: Unlicense OR MIT
-
 package main
 
-// A Gio program that demonstrates Gio widgets. See https://gioui.org for more information.
-
 import (
-	"flag"
 	"fmt"
-	"image"
-	"image/color"
-	"log"
-	"math"
-	"os"
-	"time"
 
-	"gioui.org/app"
-	"gioui.org/font/gofont"
-	"gioui.org/io/system"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/text"
-	"gioui.org/unit"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
-
-	"golang.org/x/exp/shiny/materialdesign/icons"
+	"github.com/gen2brain/raylib-go/raygui"
+	"github.com/gen2brain/raylib-go/raylib"
 )
-
-var disable = flag.Bool("disable", false, "disable all widgets")
-
-type iconAndTextButton struct {
-	theme  *material.Theme
-	button *widget.Clickable
-	icon   *widget.Icon
-	word   string
-}
 
 func main() {
-	flag.Parse()
-	ic, err := widget.NewIcon(icons.ContentAdd)
-	if err != nil {
-		log.Fatal(err)
-	}
-	icon = ic
-	progressIncrementer = make(chan int)
+	screenWidth := int32(800)
+	screenHeight := int32(450)
 
-	go func() {
-		for {
-			time.Sleep(time.Second)
-			progressIncrementer <- 10
-		}
-	}()
+	rl.SetConfigFlags(rl.FlagVsyncHint)
 
-	go func() {
-		w := app.NewWindow(app.Size(unit.Dp(800), unit.Dp(700)))
-		if err := loop(w); err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
-	app.Main()
-}
-func loop(w *app.Window) error {
-	th := material.NewTheme(gofont.Collection())
+	rl.InitWindow(screenWidth, screenHeight, "raylib [gui] example - basic controls")
 
-	var ops op.Ops
-	for {
-		select {
-		case e := <-w.Events():
-			switch e := e.(type) {
-			case system.ClipboardEvent:
-				lineEditor.SetText(e.Text)
-			case system.DestroyEvent:
-				return e.Err
-			case system.FrameEvent:
-				gtx := layout.NewContext(&ops, e)
-				for iconButton.Clicked() {
-					w.WriteClipboard(lineEditor.Text())
-				}
-				for flatBtn.Clicked() {
-					w.ReadClipboard()
-				}
-				if *disable {
-					gtx = gtx.Disabled()
-				}
-				kitchen(gtx, th)
-				e.Frame(gtx.Ops)
+	buttonToggle := true
+	buttonClicked := false
+	checkboxChecked := false
+
+	spinnerValue := 5
+	sliderValue := float32(10)
+	sliderBarValue := float32(70)
+	progressValue := float32(0.5)
+
+	comboActive := 0
+	toggleActive := 0
+
+	toggleText := []string{"Item 1", "Item 2", "Item 3"}
+	comboText := []string{"default_light", "default_dark", "hello_kitty", "monokai", "obsidian", "solarized_light", "solarized", "zahnrad"}
+
+	var inputText string
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		if buttonClicked {
+			progressValue += 0.1
+			if progressValue >= 1.1 {
+				progressValue = 0.0
 			}
-		case p := <-progressIncrementer:
-			progress += p
-			if progress > 100 {
-				progress = 0
-			}
-			w.Invalidate()
 		}
-	}
-}
 
-var (
-	editor     = new(widget.Editor)
-	lineEditor = &widget.Editor{
-		SingleLine: true,
-		Submit:     true,
-	}
-	button            = new(widget.Clickable)
-	greenButton       = new(widget.Clickable)
-	iconTextButton    = new(widget.Clickable)
-	iconButton        = new(widget.Clickable)
-	flatBtn           = new(widget.Clickable)
-	disableBtn        = new(widget.Clickable)
-	radioButtonsGroup = new(widget.Enum)
-	list              = &layout.List{
-		Axis: layout.Vertical,
-	}
-	progress            = 0
-	progressIncrementer chan int
-	green               = true
-	topLabel            = "Hello, Gio"
-	icon                *widget.Icon
-	checkbox            = new(widget.Bool)
-	swtch               = new(widget.Bool)
-	transformTime       time.Time
-	float               = new(widget.Float)
-)
+		rl.BeginDrawing()
 
-type (
-	D = layout.Dimensions
-	C = layout.Context
-)
+		rl.ClearBackground(rl.Beige)
 
-func (b iconAndTextButton) Layout(gtx layout.Context) layout.Dimensions {
-	return material.ButtonLayout(b.theme, b.button).Layout(gtx, func(gtx C) D {
-		return layout.UniformInset(unit.Dp(12)).Layout(gtx, func(gtx C) D {
-			iconAndLabel := layout.Flex{Axis: layout.Horizontal, Alignment: layout.Middle}
-			textIconSpacer := unit.Dp(5)
+		raygui.Label(rl.NewRectangle(50, 50, 80, 20), "Label")
 
-			layIcon := layout.Rigid(func(gtx C) D {
-				return layout.Inset{Right: textIconSpacer}.Layout(gtx, func(gtx C) D {
-					var d D
-					if icon != nil {
-						size := gtx.Px(unit.Dp(56)) - 2*gtx.Px(unit.Dp(16))
-						b.icon.Layout(gtx, unit.Px(float32(size)))
-						d = layout.Dimensions{
-							Size: image.Point{X: size, Y: size},
-						}
-					}
-					return d
-				})
-			})
+		buttonClicked = raygui.Button(rl.NewRectangle(50, 70, 80, 40), "Button")
 
-			layLabel := layout.Rigid(func(gtx C) D {
-				return layout.Inset{Left: textIconSpacer}.Layout(gtx, func(gtx C) D {
-					l := material.Body1(b.theme, b.word)
-					l.Color = b.theme.Color.InvText
-					return l.Layout(gtx)
-				})
-			})
+		raygui.Label(rl.NewRectangle(70, 140, 20, 20), "Checkbox")
+		checkboxChecked = raygui.CheckBox(rl.NewRectangle(50, 140, 20, 20), checkboxChecked)
 
-			return iconAndLabel.Layout(gtx, layIcon, layLabel)
-		})
-	})
-}
+		raygui.Label(rl.NewRectangle(50, 190, 200, 20), "ProgressBar")
+		raygui.ProgressBar(rl.NewRectangle(50, 210, 200, 20), progressValue)
+		raygui.Label(rl.NewRectangle(200+50+5, 210, 20, 20), fmt.Sprintf("%.1f", progressValue))
 
-func kitchen(gtx layout.Context, th *material.Theme) layout.Dimensions {
-	for _, e := range lineEditor.Events() {
-		if e, ok := e.(widget.SubmitEvent); ok {
-			topLabel = e.Text
-			lineEditor.SetText("")
-		}
-	}
-	widgets := []layout.Widget{
-		material.H3(th, topLabel).Layout,
-		func(gtx C) D {
-			gtx.Constraints.Max.Y = gtx.Px(unit.Dp(200))
-			return material.Editor(th, editor, "Hint").Layout(gtx)
-		},
-		func(gtx C) D {
-			e := material.Editor(th, lineEditor, "Hint")
-			e.Font.Style = text.Italic
-			border := widget.Border{Color: color.RGBA{A: 0xff}, CornerRadius: unit.Dp(8), Width: unit.Px(2)}
-			return border.Layout(gtx, func(gtx C) D {
-				return layout.UniformInset(unit.Dp(8)).Layout(gtx, e.Layout)
-			})
-		},
-		func(gtx C) D {
-			in := layout.UniformInset(unit.Dp(8))
-			return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-				layout.Rigid(func(gtx C) D {
-					return in.Layout(gtx, material.IconButton(th, iconButton, icon).Layout)
-				}),
-				layout.Rigid(func(gtx C) D {
-					return in.Layout(gtx, iconAndTextButton{theme: th, icon: icon, word: "Icon", button: iconTextButton}.Layout)
-				}),
-				layout.Rigid(func(gtx C) D {
-					return in.Layout(gtx, func(gtx C) D {
-						for button.Clicked() {
-							green = !green
-						}
-						return material.Button(th, button, "Click me!").Layout(gtx)
-					})
-				}),
-				layout.Rigid(func(gtx C) D {
-					return in.Layout(gtx, func(gtx C) D {
-						l := "Green"
-						if !green {
-							l = "Blue"
-						}
-						btn := material.Button(th, greenButton, l)
-						if green {
-							btn.Background = color.RGBA{A: 0xff, R: 0x9e, G: 0x9d, B: 0x24}
-						}
-						return btn.Layout(gtx)
-					})
-				}),
-				layout.Rigid(func(gtx C) D {
-					return in.Layout(gtx, func(gtx C) D {
-						return material.Clickable(gtx, flatBtn, func(gtx C) D {
-							return layout.UniformInset(unit.Dp(12)).Layout(gtx, func(gtx C) D {
-								flatBtnText := material.Body1(th, "Flat")
-								if gtx.Queue == nil {
-									flatBtnText.Color.A = 150
-								}
-								return layout.Center.Layout(gtx, flatBtnText.Layout)
-							})
-						})
-					})
-				}),
-			)
-		},
-		material.ProgressBar(th, progress).Layout,
-		func(gtx C) D {
-			return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-				layout.Rigid(func(gtx C) D {
-					return layout.Inset{Left: unit.Dp(16)}.Layout(gtx,
-						material.Switch(th, swtch).Layout,
-					)
-				}),
-				layout.Rigid(func(gtx C) D {
-					return layout.Inset{Left: unit.Dp(16)}.Layout(gtx, func(gtx C) D {
-						text := "enabled"
-						if !swtch.Value {
-							text = "disabled"
-							gtx = gtx.Disabled()
-						}
-						btn := material.Button(th, disableBtn, text)
-						return btn.Layout(gtx)
-					})
-				}),
-				layout.Rigid(func(gtx C) D {
-					return layout.Inset{Left: unit.Dp(16)}.Layout(gtx, func(gtx C) D {
-						if !swtch.Value {
-							return D{}
-						}
-						return material.Loader(th).Layout(gtx)
-					})
-				}),
-			)
-		},
-		func(gtx C) D {
-			return layout.Flex{}.Layout(gtx,
-				layout.Rigid(material.RadioButton(th, radioButtonsGroup, "r1", "RadioButton1").Layout),
-				layout.Rigid(material.RadioButton(th, radioButtonsGroup, "r2", "RadioButton2").Layout),
-				layout.Rigid(material.RadioButton(th, radioButtonsGroup, "r3", "RadioButton3").Layout),
-			)
-		},
-		func(gtx C) D {
-			return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-				layout.Flexed(1, material.Slider(th, float, 0, 2*math.Pi).Layout),
-				layout.Rigid(func(gtx C) D {
-					return layout.UniformInset(unit.Dp(8)).Layout(gtx,
-						material.Body1(th, fmt.Sprintf("%.2f", float.Value)).Layout,
-					)
-				}),
-			)
-		},
+		raygui.Label(rl.NewRectangle(50, 260, 200, 20), "Slider")
+		sliderValue = raygui.Slider(rl.NewRectangle(50, 280, 200, 20), sliderValue, 0, 100)
+		raygui.Label(rl.NewRectangle(200+50+5, 280, 20, 20), fmt.Sprintf("%.0f", sliderValue))
+
+		buttonToggle = raygui.ToggleButton(rl.NewRectangle(50, 350, 100, 40), "ToggleButton", buttonToggle)
+
+		raygui.Label(rl.NewRectangle(500, 50, 200, 20), "ToggleGroup")
+		toggleActive = raygui.ToggleGroup(rl.NewRectangle(500, 70, 60, 30), toggleText, toggleActive)
+
+		raygui.Label(rl.NewRectangle(500, 120, 200, 20), "SliderBar")
+		sliderBarValue = raygui.SliderBar(rl.NewRectangle(500, 140, 200, 20), sliderBarValue, 0, 100)
+		raygui.Label(rl.NewRectangle(500+200+5, 140, 20, 20), fmt.Sprintf("%.0f", sliderBarValue))
+
+		raygui.Label(rl.NewRectangle(500, 190, 200, 20), "Spinner")
+		spinnerValue = raygui.Spinner(rl.NewRectangle(500, 210, 200, 20), spinnerValue, 0, 100)
+
+		raygui.Label(rl.NewRectangle(500, 260, 200, 20), "ComboBox")
+		comboActive = raygui.ComboBox(rl.NewRectangle(500, 280, 200, 20), comboText, comboActive)
+
+		raygui.Label(rl.NewRectangle(500, 330, 200, 20), "TextBox")
+		inputText = raygui.TextBox(rl.NewRectangle(500, 350, 200, 20), inputText)
+
+		rl.EndDrawing()
 	}
 
-	return list.Layout(gtx, len(widgets), func(gtx C, i int) D {
-		return layout.UniformInset(unit.Dp(16)).Layout(gtx, widgets[i])
-	})
+	rl.CloseWindow()
 }
